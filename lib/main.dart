59c5dbb84e9947c85e11ab8947e6ca10b26845f3
pxthaphi <ohmzaza21@gmail.com>
2024-03-19 21:22:18 +0700

    เพิ่ม Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -15,26 +15,47 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'PK SHOP',
-      theme: ThemeData(
-      ),
-      home: FlutterSplashScreen.scale(
-          gradient: const LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              Colors.lightBlue,
-              Colors.blue,
-            ],
-          ),
-          childWidget: SizedBox(
-            height: 100,
-            child: Image.asset("assets/images/logo-pkshop.png"),
-          ),
-          duration: const Duration(milliseconds: 2000),
-          animationDuration: const Duration(milliseconds: 1000),
-          onAnimationEnd: () => debugPrint("On Scale End"),
-          nextScreen: const LoginScreen(),
+      theme: ThemeData(),
+      home: FlutterSplashScreen.gif(
+        gifPath: 'assets/images/logo.gif',
+        gifWidth: 720,
+        gifHeight: 1280,
+        gradient: const LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color.fromARGB(255, 255, 255, 255),
+            Color.fromARGB(255, 255, 255, 255),
+          ],
         ),
+        nextScreen: const LoginScreen(),
+        duration: const Duration(milliseconds: 6000),
+        onInit: () async {
+          // debugPrint("onInit");
+        },
+        onEnd: () async {
+          // debugPrint("onEnd 1");
+        },
+      ),
+
+      // home: FlutterSplashScreen.scale(
+      //     gradient: const LinearGradient(
+      //       begin: Alignment.topCenter,
+      //       end: Alignment.bottomCenter,
+      //       colors: [
+      //         Colors.lightBlue,
+      //         Colors.blue,
+      //       ],
+      //     ),
+      //     childWidget: SizedBox(
+      //       height: 100,
+      //       child: Image.asset("assets/images/logo-pkshop.png"),
+      //     ),
+      //     duration: const Duration(milliseconds: 2000),
+      //     animationDuration: const Duration(milliseconds: 1000),
+      //     onAnimationEnd: () => debugPrint("On Scale End"),
+      //     nextScreen: const LoginScreen(),
+      //   ),
     );
   }
 }
